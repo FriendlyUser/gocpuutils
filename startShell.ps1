710ba0a69b3ecb20a2f1c8a46289d6d1590bd785
dli <davidli012345@gmail.com>
2019-10-26 15:38:19 -0700

    trying again

--- startShell.ps1
+++ startShell.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy RemoteSigned
 choco install -y nodejs
 choco install -y mingw
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") || true
