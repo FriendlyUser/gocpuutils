cec4d67bed6293bf3f8d51275226e5cefd2e35b7
dli <davidli012345@gmail.com>
2019-10-26 15:50:56 -0700

    trying to reset

--- startShell.ps1
+++ startShell.ps1
@@ -1,5 +1,5 @@
 choco install -y nodejs
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") || true
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 node --version || true || npm --version
 go get ./...
 go get github.com/wailsapp/wails/cmd/wails
