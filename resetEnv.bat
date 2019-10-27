00802cd7eeebdac7c7b5219ee0528cf33db924c4
dli <davidli012345@gmail.com>
2019-10-26 20:16:32 -0700

    proper resest

--- resetEnv.bat
+++ resetEnv.bat
@@ -1,10 +1,6 @@
 @echo off
 echo.
 echo Refreshing PATH from registry
-SET PATH=%PATH%;c:gopath\bin
-SET PATH=%PATH%;c:\gopath
-SET PATH=%PATH%;c:gopath\bin\wails.exe
-SET PATH=%PATH%;c:gopath\bin\mewn.exe
 :: Get System PATH
 for /f "tokens=3*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path') do set syspath=%%A%%B
 
@@ -13,6 +9,9 @@ for /f "tokens=3*" %%A in ('reg query "HKCU\Environment" /v Path') do set userpa
 
 :: Set Refreshed Path
 set PATH=%userpath%;%syspath%
-
+SET PATH=%PATH%;c:gopath\bin
+SET PATH=%PATH%;c:\gopath
+SET PATH=%PATH%;c:gopath\bin\wails.exe
+SET PATH=%PATH%;c:gopath\bin\mewn.exe
 echo Refreshed PATH
 echo %PATH%
